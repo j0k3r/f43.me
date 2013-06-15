9f227607a402dc9441d549050ec0db99a302d7cc
Jeremy <jeremy.benoist@gmail.com>
2013-06-15 17:07:35 +0200

    Secure the app
    Everything is restricted except the public view.

---
+++ src/j0k3r/FeedBundle/Controller/SecurityController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace j0k3r\FeedBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\SecurityContext;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+/**
+ * Security controller.
+ */
+class SecurityController extends Controller
+{
+    /**
+     * Display some information about feeds, items, logs, etc ...
+     *
+     * @Template()
+     *
+     * @return array
+     */
+    public function loginAction()
+    {
+        if (true === $this->get('security.context')->isGranted('ROLE_ADMIN')) {
+            return $this->redirect($this->generateUrl('j0k3r_feed_dashboard'));
+        }
+
+        $request = $this->getRequest();
+        $session = $request->getSession();
+
+        // get the login error if there is one
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(
+                SecurityContext::AUTHENTICATION_ERROR
+            );
+        } else {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+        }
+
+        return array(
+            // last username entered by the user
+            'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+            'error'         => $error,
+        );
+    }
+}
