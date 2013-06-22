691b0f60791c874eb1c04c7578a61c6d1848b4f8
Jeremy <jeremy.benoist@gmail.com>
2013-06-22 12:56:26 +0200

    Change the way to ping hub
    Remove multi curl and use one curl with many urls (since the hub accept multiple urls at once)

--- src/j0k3r/FeedBundle/EventListener/FeedItemSubscriber.php
+++ src/j0k3r/FeedBundle/EventListener/FeedItemSubscriber.php
@@ -19,60 +19,44 @@ class FeedItemSubscriber
      * http://nathangrigg.net/2012/09/real-time-publishing/
      *
      * @param   FeedItemEvent $event
-     * @return  void
+     * @return  true/false
      */
     public function pingHub(FeedItemEvent $event)
     {
         if (empty($this->hub)) {
-            return;
+            return false;
         }
 
         // retrieve feed urls
         $urls = $event->getFeedUrls();
 
-        // ping them all !
-        // http://www.fusionswift.com/2011/08/php-curl_multi_exec-example/
-        $curl_array = array();
-        $ch = curl_multi_init();
-
-        foreach($urls as $key => $url) {
-            $curl_array[$key] = curl_init();
-
-            $data = array(
-                'hub.mode' => 'publish',
-                'hub.url'  => $url,
-            );
-
-            curl_setopt($curl_array[$key], CURLOPT_URL, $this->hub);
-            curl_setopt($curl_array[$key], CURLOPT_RETURNTRANSFER, TRUE);
-            curl_setopt($curl_array[$key], CURLOPT_HEADER, TRUE);
-            curl_setopt($curl_array[$key], CURLOPT_NOBODY, TRUE);
-            curl_setopt($curl_array[$key], CURLOPT_POST, true);
-            curl_setopt($curl_array[$key], CURLOPT_POSTFIELDS, $data);
-
-            curl_multi_add_handle($ch, $curl_array[$key]);
+        // ping publisher
+        // https://code.google.com/p/pubsubhubbub/source/browse/trunk/publisher_clients/php/library/publisher.php
+        $params = "hub.mode=publish";
+        foreach ($urls as $url) {
+            $params .= "&hub.url=" . urlencode($url);
         }
 
-        do {
-            curl_multi_exec($ch, $exec);
-        } while($exec > 0);
+        // make the request
+        $options = array(
+            CURLOPT_URL        => $this->hub,
+            CURLOPT_POST       => true,
+            CURLOPT_POSTFIELDS => $params,
+            CURLOPT_USERAGENT  => "PubSubHubbub-Publisher-PHP/1.0"
+        );
 
-        foreach($urls as $key => $url) {
-            $response = curl_multi_getcontent($curl_array[$key]);
+        $ch = curl_init();
+        curl_setopt_array($ch, $options);
+        $response = curl_exec($ch);
 
-            if (!preg_match('/(HTTP\/1\.1 204 No Content)/i', $response)) {
-                // hub doesn't respond correctly, do something ?
-            }
+        // receive the response
+        $info = curl_getinfo($ch);
+        curl_close($ch);
+
+        if ($info['http_code'] != 204) {
+            // hub doesn't respond correctly, do something ?
+            return false;
         }
-
-        foreach($urls as $key => $url) {
-            curl_multi_remove_handle($ch, $curl_array[$key]);
-        }
-
-        curl_multi_close($ch);
-
-        foreach($urls as $key => $url) {
-            curl_close($curl_array[$key]);
-        }
+        return true;
     }
 }
