04d9c2cbf8a1f8dfdbdbc2ce1f9b720f82db88d1
Jeremy <jeremy.benoist@gmail.com>
2013-07-04 23:41:14 +0200

    Add one alternative to feed validator
    This evening, it seems that validator.w3.org is down. Using feedvalidator.org, it adds one alternative.

--- src/j0k3r/FeedBundle/Validator/Constraints/ContainsRssValidator.php
+++ src/j0k3r/FeedBundle/Validator/Constraints/ContainsRssValidator.php
@@ -24,7 +24,22 @@ class ContainsRssValidator extends ConstraintValidator
         $content = curl_exec($ch);
         curl_close($ch);
 
-        if (0 === preg_match('/This is a valid/', $content)) {
+        // if content is false, it means the request timeout, let's try with an alternative
+        if (false === $content) {
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, 'http://feedvalidator.org/check.cgi?url='.$value);
+            curl_setopt($ch, CURLOPT_HEADER, false);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_USERAGENT, 'f43.me');
+            // wait 3s before time out
+            curl_setopt($ch, CURLOPT_TIMEOUT, 3);
+            $content = curl_exec($ch);
+            curl_close($ch);
+        }
+
+        // if content is still false, we won't invalidate the feed, it means both alternative to check are unavailable
+        if (false !== $content && 0 === preg_match('/This is a valid/', $content)) {
             $this->context->addViolation($constraint->message, array('%string%' => $value));
         }
     }
