314ed33dd26371e7abb7f830632dda533030aed4
Jeremy <j0k3r@users.noreply.github.com>
2013-09-06 09:42:15 +0200

    Revert 5af237f95082f3d3d55ec766a8ebed7a58327e7c
    We are moving the permalink verification when we create the document, not after.

--- src/j0k3r/FeedBundle/Repository/FeedItemRepository.php
+++ src/j0k3r/FeedBundle/Repository/FeedItemRepository.php
@@ -108,7 +108,7 @@ class FeedItemRepository extends DocumentRepository
     public function getAllLinks($feedId)
     {
         $res = $this->createQueryBuilder()
-            ->select('permalink', 'link')
+            ->select('permalink')
             ->hydrate(false)
             ->field('feed.id')->equals($feedId)
             ->sort('published_at', 'DESC')
@@ -119,9 +119,7 @@ class FeedItemRepository extends DocumentRepository
         // and also because it's faster to isset than in_array to match a value
         $results = array();
         foreach ($res as $item) {
-            // sometimes permalink can't be define in the source feed, use link instead
-            $link = (isset($item['permalink']) && $item['permalink']) ? $item['permalink'] : $item['link'];
-            $results[$link] = true;
+            $results[$item['permalink']] = true;
         }
 
         return $results;
