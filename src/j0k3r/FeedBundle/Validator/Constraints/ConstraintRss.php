80fb87fe00c8e5afa89b5a47c2a6d715c26bd26b
Jeremy <jeremy.benoist@gmail.com>
2014-09-05 19:10:03 +0200

    Fix annotation

--- src/j0k3r/FeedBundle/Validator/Constraints/ConstraintRss.php
+++ src/j0k3r/FeedBundle/Validator/Constraints/ConstraintRss.php
@@ -3,7 +3,11 @@
 namespace j0k3r\FeedBundle\Validator\Constraints;
 
 use Symfony\Component\Validator\Constraint;
+use Doctrine\Common\Annotations\Annotation;
 
+/**
+ * @Annotation
+ */
 class ConstraintRss extends Constraint
 {
     public $message = 'Feed "%string%" is not valid.';
