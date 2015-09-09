803c6ced35949de893d247998178fe287d72ae16
Jeremy Benoist <jeremy.benoist@gmail.com>
2015-09-09 19:45:26 +0200

    Use graby instead of Readability
    Remove custom check from Readability
    Remove Tubelink since job is now done by graby

--- src/FeedBundle/Tests/Parser/InternalTest.php
+++ src/FeedBundle/Tests/Parser/InternalTest.php
@@ -10,112 +10,63 @@ use GuzzleHttp\Stream\Stream;
 
 class InternalTest extends \PHPUnit_Framework_TestCase
 {
-    protected $regexs = array(
-        'unlikelyCandidates' => '/combx|comment|community|disqus|extra|foot|header|menu|remark|rss|shoutbox|sidebar|sponsor|ad-break|agegate|pagination|pager|popup|addthis|response|slate_associated_bn|reseaux|sharing|auteur|tag|feedback|meta|kudo|sidebar|copyright|bio|moreInfo|legal|share/i',
-        'okMaybeItsACandidate' => '/and|article|body|column|main|shadow/i',
-        'positive' => '/article|body|content|entry|hentry|main|page|attachment|pagination|post|text|blog|story/i',
-        'negative' => '/combx|comment|com-|contact|foot|footer|_nav|footnote|masthead|media|meta|outbrain|promo|related|scroll|shoutbox|sidebar|sponsor|shopping|tags|tool|widget|header|aside/i',
-        'divToPElements' => '/<(a|blockquote|dl|div|img|ol|p|pre|table|ul)/i',
-        'replaceBrs' => '/(<br[^>]*>[ \n\r\t]*){2,}/i',
-        'replaceFonts' => '/<(\/?)font[^>]*>/i',
-        'normalize' => '/\s{2,}/',
-        'killBreaks' => '/(<br\s*\/?>(\s|&nbsp;?)*){1,}/',
-        'video' => '!//(player\.|www\.)?(youtube|vimeo|viddler|dailymotion)\.com!i',
-        'skipFootnoteLink' => '/^\s*(\[?[a-z0-9]{1,2}\]?|^|edit|citation needed)\s*$/i',
-        'attrToRemove' => 'onclick|rel|class|target|fs:definition|alt|id|onload|name|onchange',
-        'tagToRemove' => 'select|form|header|footer|aside',
-    );
-
     public function testParseEmpty()
     {
-        $client = new Client();
+        $graby = $this->getMockBuilder('Graby\Graby')
+            ->setMethods(array('fetchContent'))
+            ->disableOriginalConstructor()
+            ->getMock();
 
-        $mock = new Mock([
-            new Response(200, []),
-        ]);
+        $graby->expects($this->any())
+            ->method('fetchContent')
+            ->willReturn(false);
 
-        $client->getEmitter()->attach($mock);
-
-        $internal = new Internal($client, $this->regexs);
+        $internal = new Internal($graby);
         $this->assertEmpty($internal->parse('http://localhost'));
-    }
-
-    public function testParse()
-    {
-        $client = new Client();
-
-        $mock = new Mock([
-            new Response(200, ['Content-Encoding' => 'deflate', 'Content-Type' => 'text/html; charset=iso'], Stream::factory('<div></div>')),
-        ]);
-
-        $client->getEmitter()->attach($mock);
-
-        $internal = new Internal($client, $this->regexs);
-        $this->assertEmpty($internal->parse('http://localhost'));
-    }
-
-    public function testParseVideo()
-    {
-        $internal = new Internal(new Client(), $this->regexs);
-        $this->assertContains('<iframe src="http://www.youtube.com/embed/8b7t5iUV0pQ" width="560" height="315"', $internal->parse('https://www.youtube.com/watch?v=8b7t5iUV0pQ'));
-    }
-
-    /**
-     * This will throw an exception but the fallback will try to retrieve content using file_get_contents.
-     */
-    public function testParseGuzzleException()
-    {
-        $client = new Client();
-
-        $mock = new Mock([
-            new Response(400, [], Stream::factory('oops')),
-        ]);
-
-        $client->getEmitter()->attach($mock);
-
-        $internal = new Internal($client, $this->regexs);
-        $this->assertEmpty($internal->parse('http://foo.bar.youpla'));
     }
 
     public function testParseFalse()
     {
-        $client = new Client();
+        $graby = $this->getMockBuilder('Graby\Graby')
+            ->setMethods(array('fetchContent'))
+            ->disableOriginalConstructor()
+            ->getMock();
 
-        $mock = new Mock([
-            new Response(200, ['Content-Type' => 'text']),
-        ]);
+        $graby->expects($this->any())
+            ->method('fetchContent')
+            ->willReturn(array('html' => false));
 
-        $client->getEmitter()->attach($mock);
-
-        $internal = new Internal($client, $this->regexs);
+        $internal = new Internal($graby);
         $this->assertEmpty($internal->parse('http://localhost'));
     }
 
-    public function testParseImage()
+    public function testParseOk()
     {
-        $client = new Client();
+        $graby = $this->getMockBuilder('Graby\Graby')
+            ->setMethods(array('fetchContent'))
+            ->disableOriginalConstructor()
+            ->getMock();
 
-        $mock = new Mock([
-            new Response(200, ['Content-Encoding' => 'deflate', 'Content-Type' => 'image'], Stream::factory('<div></div>')),
-        ]);
+        $graby->expects($this->any())
+            ->method('fetchContent')
+            ->willReturn(array('html' => '<p>test</p>'));
 
-        $client->getEmitter()->attach($mock);
-
-        $internal = new Internal($client, $this->regexs);
-        $this->assertEquals('<img src="http://localhost" />', $internal->parse('http://localhost'));
+        $internal = new Internal($graby);
+        $this->assertNotEmpty($internal->parse('http://localhost'));
     }
 
-    public function testParseGzip()
+    public function testParseException()
     {
-        $client = new Client();
+        $graby = $this->getMockBuilder('Graby\Graby')
+            ->setMethods(array('fetchContent'))
+            ->disableOriginalConstructor()
+            ->getMock();
 
-        $mock = new Mock([
-            new Response(200, ['Content-Encoding' => 'gzip', 'Content-Type' => 'text'], Stream::factory(gzencode("<p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte.</p>"))),
-        ]);
+        $graby->expects($this->any())
+            ->method('fetchContent')
+            ->will($this->throwException(new \Exception));
 
-        $client->getEmitter()->attach($mock);
-
-        $internal = new Internal($client, $this->regexs);
-        $this->assertContains('readability', $internal->parse('http://localhost'));
+        $internal = new Internal($graby);
+        $this->assertEmpty($internal->parse('http://localhost'));
     }
 }
