65952abdf7ec152adf60b463e2a677a596153b1f
Jeremy Benoist <jeremy.benoist@gmail.com>
2015-10-26 20:43:01 +0100

    Add custom extract for Rue89
    Since they now use an api to display content on their website.
    
    Fix #56

---
+++ src/FeedBundle/Extractor/Rue89.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Api43\FeedBundle\Extractor;
+
+use GuzzleHttp\Exception\RequestException;
+
+class Rue89 extends AbstractExtractor
+{
+    protected $rue89Id = null;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function match($url)
+    {
+        $host = parse_url($url, PHP_URL_HOST);
+        $path = parse_url($url, PHP_URL_PATH);
+
+        if (false === $host || false === $path) {
+            return false;
+        }
+
+        if (false === strpos($host, 'rue89.nouvelobs.com')) {
+            return false;
+        }
+
+        preg_match('/\-([0-9]+)$/i', $path, $matches);
+
+        if (!isset($matches[1])) {
+            return false;
+        }
+
+        $this->rue89Id = $matches[1];
+
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getContent()
+    {
+        if (!$this->rue89Id) {
+            return '';
+        }
+
+        try {
+            $data = $this->client
+                ->get('http://api.rue89.nouvelobs.com/export/mobile2/node/'.$this->rue89Id.'/full')
+                ->json();
+        } catch (RequestException $e) {
+            $this->logger->warning('Rue89 extract failed for: '.$this->rue89Id, array(
+                'exception' => $e,
+            ));
+
+            return '';
+        }
+
+        if (!is_array($data) || empty($data)) {
+            return '';
+        }
+
+        return '<div><h2>'.$data['node']['title'].'</h2><p>'.$data['node']['intro'].'</p><p><img src="'.$data['node']['imgTabletteCarousel'].'"></p>'.$data['node']['body'].'</div>';
+    }
+}
---
+++ src/FeedBundle/Tests/Extractor/Rue89Test.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Api43\FeedBundle\Tests\Extractor;
+
+use Api43\FeedBundle\Extractor\Rue89;
+use Monolog\Logger;
+use Monolog\Handler\TestHandler;
+use GuzzleHttp\Client;
+use GuzzleHttp\Subscriber\Mock;
+use GuzzleHttp\Message\Response;
+use GuzzleHttp\Stream\Stream;
+
+class Rue89Test extends \PHPUnit_Framework_TestCase
+{
+    public function dataMatch()
+    {
+        return array(
+            array('http://rue89.nouvelobs.com/2015/10/26/algorithmes-antimensonge-fin-bobards-politique-261827', true),
+            array('http://api.rue89.nouvelobs.com/2015/10/26/ils-protegent-vie-privee-autres-prejuges-les-allemands-261832', true),
+            array('https://api.rue89.nouvelobs.com/2015/10/26/ils-protegent-vie-privee-autres-prejuges-les-allemands-261832', true),
+            array('http://rue89.nouvelobs.com/2015/10/26/algorithmes-antimensonge-fin-bobards-politique-261827.html', false),
+            array('https://goog.co', false),
+            array('http://user@:80', false),
+        );
+    }
+
+    /**
+     * @dataProvider dataMatch
+     */
+    public function testMatch($url, $expected)
+    {
+        $rue89 = new Rue89();
+        $this->assertEquals($expected, $rue89->match($url));
+    }
+
+    public function testContent()
+    {
+        $client = new Client();
+
+        $mock = new Mock([
+            new Response(200, [], Stream::factory(json_encode(array('node' => array('title' => 'my title', 'intro' => 'my description', 'imgTabletteCarousel' => 'http://0.0.0.0/img.jpg', 'body' => '<iframe/>'))))),
+            new Response(200, [], Stream::factory('')),
+            new Response(400, [], Stream::factory('oops')),
+        ]);
+
+        $client->getEmitter()->attach($mock);
+
+        $rue89 = new Rue89();
+        $rue89->setClient($client);
+
+        $logHandler = new TestHandler();
+        $logger = new Logger('test', array($logHandler));
+        $rue89->setLogger($logger);
+
+        // first test fail because we didn't match an url, so Rue89Url isn't defined
+        $this->assertEmpty($rue89->getContent());
+
+        $rue89->match('http://rue89.nouvelobs.com/2015/10/26/algorithmes-antimensonge-fin-bobards-politique-261827');
+
+        // consecutive calls
+        $this->assertEquals('<div><h2>my title</h2><p>my description</p><p><img src="http://0.0.0.0/img.jpg"></p><iframe/></div>', $rue89->getContent());
+        // this one will got an empty array
+        $this->assertEmpty($rue89->getContent());
+        // this one will catch an exception
+        $this->assertEmpty($rue89->getContent());
+
+        $this->assertTrue($logHandler->hasWarning('Rue89 extract failed for: 261827'), 'Warning message matched');
+    }
+}
