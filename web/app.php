184f37b4c2c618bb0cd563dd325db8e4fc8855b3
Jeremy Benoist <jeremy.benoist@gmail.com>
2016-01-01 21:21:08 +0100

    Move to the 3.0 directory structure

--- web/app.php
+++ web/app.php
@@ -1,22 +1,27 @@
 <?php
 
-use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+/**
+ * @var Composer\Autoload\ClassLoader
+ */
+$loader = require __DIR__.'/../app/autoload.php';
+include_once __DIR__.'/../var/bootstrap.php.cache';
 
-// Use APC for autoloading to improve performance.
-// Change 'sf2' to a unique prefix in order to prevent cache key conflicts
-// with other applications also using APC.
+// Enable APC for autoloading to improve performance.
+// You should change the ApcClassLoader first argument to a unique prefix
+// in order to prevent cache key conflicts with other applications
+// also using APC.
+$apcLoader = new Symfony\Component\ClassLoader\ApcClassLoader('f43me-loader', $loader);
+$loader->unregister();
+$apcLoader->register(true);
 
-$loader = new ApcClassLoader('f43me-loader', $loader);
-$loader->register(true);
-
-require_once __DIR__.'/../app/AppCache.php';
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 $kernel = new AppCache($kernel);
+
+// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
