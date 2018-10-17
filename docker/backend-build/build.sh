0e2e0dfaa36738424a72746506c1f2fad3224d16
Jeremy Benoist <jeremy.benoist@gmail.com>
2018-10-17 23:09:48 +0200

    Only keep 7.2
    For now, the backend build container seems to not be able to communicate with the db container …

--- docker/backend-build/build.sh
+++ docker/backend-build/build.sh
@@ -7,9 +7,7 @@ chmod 777 -Rf ./var
 # Undo permission changes to .gitkeep files within var/
 git checkout ./var
 
-# The doctrine/mongodb bundle in use here is only compatible with the old style mongo extension
-# PHP 7+ needs a polyfill, but we don't want it in the regular composer.json
-composer require "alcaeus/mongo-php-adapter=^1.0.0" --ignore-platform-reqs
-
 # Load up schema in mongo
-php bin/console doctrine:mongodb:schema:create || echo "Schema already created"
+php bin/console doctrine:mongodb:schema:create
+
+echo "Schema created, container will now die. 👋"
