3117e1faf166a1c0699ab44728fb78ea9825e2ee
Jeremy Benoist <jeremy.benoist@gmail.com>
2022-07-18 15:25:33 +0200

    Fix Docker

--- docker/vhost.sh
+++ docker/vhost.sh
@@ -1,29 +1,20 @@
 echo 'server {
     listen 8100;
     server_name f43me.local;
-    root /usr/share/nginx/html/web;
+    root /usr/share/nginx/html/public;
 
-    index app.php index.html;
+    index index.php index.html;
 
     access_log /var/log/nginx/f43me-access_log;
     error_log /var/log/nginx/f43me-error_log;
 
     location / {
-        try_files $uri $uri/ /app.php?$args;
-    }
-
-    # DEV
-    # This rule should only be placed on your development environment
-    location ~ ^/(app_dev)\.php(/|$) {
-        fastcgi_pass 127.0.0.1:9000;
-        fastcgi_split_path_info ^(.+\.php)(/.*)$;
-        include /etc/nginx/fastcgi_params;
-        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+        try_files $uri $uri/ /index.php?$args;
     }
 
     location ~ \.php$ {
         try_files $uri =404;
-        fastcgi_index app.php;
+        fastcgi_index index.php;
         fastcgi_pass 127.0.0.1:9000;
         fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
         include /etc/nginx/fastcgi_params;
