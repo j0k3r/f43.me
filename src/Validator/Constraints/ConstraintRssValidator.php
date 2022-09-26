6b32334e9fa2ede15886af06a991afe1eef4eaec
Tac Tacelosky <tacman@gmail.com>
2022-09-26 14:37:55 -0400

    tweak validator so that reddit.com/.rss doesn't fail

--- src/Validator/Constraints/ConstraintRssValidator.php
+++ src/Validator/Constraints/ConstraintRssValidator.php
@@ -25,23 +25,30 @@ class ConstraintRssValidator extends ConstraintValidator
      */
     public function validate($value, Constraint $constraint): void
     {
+        // reddit feeds are not valid RSS
+        if (str_contains($value, 'reddit.com')) {
+            return;
+        }
+
         try {
             $content = $this->client
                 ->get('https://validator.w3.org/feed/check.cgi?url=' . $value)
                 ->getBody();
         } catch (RequestException $e) {
             // if thing goes wrong, let's try with an alternative
-            try {
-                $content = $this->client
-                    ->get('https://feedvalidator.org/check.cgi?url=' . $value)
-                    ->getBody();
-            } catch (RequestException $e) {
-                $content = false;
-            }
+//            try {
+//                $content = $this->client
+//                    ->get('https://feedvalidator.org/check.cgi?url=' . $value)
+//                    ->getBody();
+//            } catch (RequestException $e) {
+//                $content = false;
+//            }
+            $content = false;
         }
 
         // if content is still false, we won't invalidate the feed, it means both alternative to check are unavailable
         if (false !== $content && 0 === preg_match('/This is a valid/', $content)) {
+            dump($content);
             $this->context->addViolation('Feed "%string%" is not valid.', ['%string%' => $value]);
         }
     }
